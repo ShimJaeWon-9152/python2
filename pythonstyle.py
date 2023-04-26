bbc10b1c4a7a0c00ac50fad0c24d39e28e707d21
ShimJaeWon-9152 <wodnjs6605@naver.com>
2023-04-26 23:54:54 +0900

    Update pythonstyle.py

--- pythonstyle.py
+++ pythonstyle.py
@@ -37,6 +37,86 @@ print(result)
 """
 
 #연습문제 2번
+"""
 cat_song = "my cat has blue eyes, my cat is cute"
 print(cat_song.split())
 print({i:j for j,i in enumerate(cat_song.split())})
+for j,i in enumerate(cat_song.split()):
+    print(j)
+    print(i)
+"""
+
+#연습문제 3번
+"""
+colors = ['orange','pink','brown','black','white']
+result = '&'.join(colors)
+print(result)
+"""
+
+#연습문제 4번
+"""
+user_dict = {}
+user_list = ["student","superuser","professor","employee"]
+for value_1,value_2 in enumerate(user_list):
+    user_dict[value_2] = value_1
+print(user_dict)
+"""
+
+#문제 5번
+"""
+result1 = [i for i in range(10) if i%2 == 0]
+print(result1)
+
+items = 'zero one two three'.split("two")
+result1 = [i for i in range(10)]
+print(result1)
+
+items = 'zero one two three'.split()
+print(items)
+
+example = 'cs50.gachon.edu'
+subdomain,domain,tld = example.split('.')
+print(subdomain)
+"""
+
+#문제 6번
+"""
+animal = ['fox','dog','cat','monkey','hores','panda','owl']
+print([ani for ani in animal if 'o' not in ani])
+"""
+
+#문제 7번
+"""
+name = "Hanbit University"
+student = ["Hong","Gil","Dong"]
+split_name = name.split()
+join_student = ''.join(student)
+print(join_student[-4:]+split_name[1])
+"""
+
+#문제 8번
+
+"""
+kor_score = [49, 79, 20, 100, 80]
+math_score = [43, 59, 85, 30, 90]
+eng_score = [49, 79, 48, 60, 100]
+mid_score = [kor_score,math_score,eng_score]
+print(mid_score[0][2])
+"""
+
+#문제 9번 
+"""
+a = [1,2,3]
+b = [4,5, ]
+c = [7,8,9]
+print([[sum(k),len(k)]for k in zip(a,b,c)])
+for k in zip(a,b,c):
+    print(k)
+"""
+
+#문제 10번
+week  = ['mon','tue','wed','thu','fri','sat','sun']
+rainbow = ['red','orange','yellow','green','blue','navy','purple']
+list_data = [week, rainbow]
+
+print(list_data[1][2])
